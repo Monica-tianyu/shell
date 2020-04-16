3be1cd90a49275a8d5e7d5864d266893bd62252d
Monica-tianyu <63727463+Monica-tianyu@users.noreply.github.com>
2020-04-16 19:28:51 +0800

    Create Function.h

---
+++ Function.h
@@ -0,0 +1,9 @@
+using namespace std;
+
+void copy(char *p,char *p2);
+void compare(char *p,char *p2);
+int count_line(char *p2);
+int count_ch(char *p2);
+int count_word(char *p2);
+void cat(char *p);
+void wc(char *p);
